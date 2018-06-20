23397a006665d2523be99a9439254f6623834a68
Dominik Ryńko <dominikrynko@gmail.com>
2018-06-20 22:20:08 +0200

    Code clean up

--- src/core/Router.php
+++ src/core/Router.php
@@ -20,14 +20,14 @@ class Router
 
         if (!is_string($step) || is_null($step)) {
             throw new \Exception('$step variable must be INT type and cannot be NULL!');
-        } else {
-            $pathToFile = TEMPLATES_PATH . $step . APP_PHP;
+        }
 
-            if (file_exists($pathToFile) && filesize($pathToFile) !== 0) {
-                include $pathToFile;
-            } else {
-                throw new RouterException('File: ' . $pathToFile . 'does not exist or is empty.');
-            }
+        $pathToFile = TEMPLATES_PATH . $step . APP_PHP;
+
+        if (file_exists($pathToFile) && filesize($pathToFile) !== 0) {
+            include $pathToFile;
+        } else {
+            throw new RouterException('File: ' . $pathToFile . ' does not exist or is empty.');
         }
     }
 }
