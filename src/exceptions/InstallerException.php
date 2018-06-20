ee2e9e3b854307266b57875214ee3750638179c4
Dominik Ryńko <dominikrynko@gmail.com>
2018-06-20 22:19:21 +0200

    Comments improve

--- src/exceptions/InstallerException.php
+++ src/exceptions/InstallerException.php
@@ -2,11 +2,13 @@
 
 namespace SystemInstaller\Exceptions;
 
+use Exception;
+
 /**
  * Class InstallerException
  * @package SystemInstaller
 */
-class InstallerException extends \Exception
+class InstallerException extends Exception
 {
-    // Your exception here
+    // Custom exception here
 }
